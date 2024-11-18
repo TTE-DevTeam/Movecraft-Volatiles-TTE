9f917882811ffcf64cbce81b82ba55d5bc26bfc7
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-18 19:44:56 +0100

    implement all listeners

--- src/main/java/me/goodroach/movecraftvolatiles/listener/IgnitionListener.java
+++ src/main/java/me/goodroach/movecraftvolatiles/listener/IgnitionListener.java
@@ -19,6 +19,8 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Waterlogged;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,6 +28,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.TNTPrimeEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -34,6 +37,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class IgnitionListener implements Listener {
@@ -48,17 +52,23 @@ public class IgnitionListener implements Listener {
             final Class<?> magicNumbersClass = Class.forName(Bukkit.getServer().getClass().getPackage().getName() + ".util.CraftMagicNumbers");
             blockRetrievalMethod = magicNumbersClass.getMethod("getBlock", Material.class);
             Object fireBlockInstance = blockRetrievalMethod.invoke(Material.FIRE);
-            igniteOddsMap = getFieldValueSafe(fireBlockInstance, "igniteOdds");
+            igniteOddsMap = getFieldValue(fireBlockInstance, "igniteOdds");
 
             return (block) -> {
                 if (igniteOddsMap != null && blockRetrievalMethod != null) {
-                    Object blockObj = blockRetrievalMethod.invoke(block.getType());
+                    Object blockObj = null;
+                    try {
+                        blockObj = blockRetrievalMethod.invoke(block.getType());
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        return block.getType().isBurnable();
+                    }
                     return igniteOddsMap.getInt(blockObj) > 0;
                 } else {
                     return block.getType().isBurnable();
                 }
             };
-        } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+        } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException | IllegalAccessException |
+                 NoSuchFieldException e) {
             return (b) -> {
                 return b.getType().isBurnable();
             };
@@ -102,95 +112,50 @@ public class IgnitionListener implements Listener {
         return null;
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onBlockBurnt(BlockBurnEvent event) {
-        Block block = event.getBlock();
-
-
-    }
-
-    // Support volatile tnt too!
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onTNTPrimeEvent(TNTPrimeEvent event) {
-        if (event.getCause() != TNTPrimeEvent.PrimeCause.FIRE || event.getBlock() == null) {
-            return;
+    protected static boolean handleVolatile(Consumer<Boolean> setEventCancelled, Block affectedBlock, Entity cause, final VolatileBlock.EReactionType eventType {
+        VolatileBlock volatileBlock = MovecraftVolatiles.getInstance().getVolatilesManager().getVolatileBlock(affectedBlock.getType());
+        if (volatileBlock == null) {
+            return false;
         }
 
-        final Block block = event.getBlock();
-        final Location blockLocation = block.getLocation();
-        VolatileBlock volatileBlock = MovecraftVolatiles.getInstance().getVolatilesManager().getVolatileBlock(block.getType());
-        if (volatileBlock == null) {
-            return;
+        if (!eventType.coveredByMask(volatileBlock)) {
+            return false;
         }
 
         double randomNumber = Math.random();
-        if (volatileBlock.getExplosionProbability() < randomNumber) {
-            return;
+        if (volatileBlock.explosionProbability() < randomNumber) {
+            return false;
         }
 
-        final boolean explosionSuccessful = block.getWorld().createExplosion(blockLocation, (float) volatileBlock.getExplosivePower(), volatileBlock.isIncendiary());
-        if (explosionSuccessful) {
-            event.setCancelled(true);
+        final Block nextBlock = getNextBurnableBlock(affectedBlock);
+        if (nextBlock != null) {
+            return false;
+        }
+        final Craft craft = fastNearestPlayerCraftToLoc(nextBlock.getLocation());
+        if (volatileBlock.requiresCraft()) {
+            if (craft == null) {
+                return false;
+            }
+            if (!craft.getHitBox().contains(MathUtils.bukkit2MovecraftLoc(nextBlock.getLocation()))) {
+                return false;
+            }
+        }
 
-            final Craft craft = fastNearestPlayerCraftToLoc(blockLocation);
+        final boolean explosionSuccessful = nextBlock.getWorld().createExplosion(nextBlock.getLocation(), (float) volatileBlock.explosivePower(), volatileBlock.isIncendiary());
+        if (explosionSuccessful) {
+            setEventCancelled.accept(true);
+
             if (craft != null && craft instanceof PlayerCraft) {
-                if (event.getPrimingEntity() != null) {
-                    damagedCraft((PlayerCraft) craft, event.getPrimingEntity().getUniqueId());
+                if (cause != null) {
+                    damagedCraft((PlayerCraft) craft, cause.getUniqueId());
                 }
             }
+            return true;
         }
+        return false;
     }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onIgnite(BlockIgniteEvent event) {
-        if (event.getCause() != BlockIgniteEvent.IgniteCause.FIREBALL)
-            return;
-
-        @Nullable
-        Player ignitingPlayer = null;
-        if (event.getIgnitingEntity() instanceof Player) {
-            ignitingPlayer = (Player) event.getIgnitingEntity();
-            if (ignitingPlayer.getInventory().getItemInMainHand().getType() == Material.FIRE_CHARGE ||
-                    ignitingPlayer.getInventory().getItemInOffHand().getType() == Material.FIRE_CHARGE) {
-                return;
-            }
-        }
-
-        Block sourceBlock = event.getBlock();
-        Block testBlock = getNextBurnableBlock(sourceBlock);
-        if (testBlock == null) {
-            return;
-        }
-
-        Location blockLocation = testBlock.getLocation();
-
-        Craft craft = fastNearestPlayerCraftToLoc(blockLocation);
-        if (craft == null || event.isCancelled()) {
-            return;
-        }
-
-        if (!craft.getHitBox().contains(MathUtils.bukkit2MovecraftLoc(blockLocation))) {
-            return;
-        }
-
-        Material material = testBlock.getType();
-        if (!MovecraftVolatiles.getInstance().getVolatilesManager().isVolatileBlock(material)) {
-            return;
-        }
-
-        double randomNumber = Math.random();
-        VolatileBlock volatileBlock = MovecraftVolatiles.getInstance().getVolatilesManager().getVolatileBlock(material);
-        if (volatileBlock.getExplosionProbability() < randomNumber) {
-            return;
-        }
-
-        craft.getWorld().createExplosion(blockLocation, (float) volatileBlock.getExplosivePower(), volatileBlock.isIncendiary());
-        if (craft instanceof PlayerCraft && ignitingPlayer != null) {
-            damagedCraft((PlayerCraft) craft, ignitingPlayer.getUniqueId());
-        }
-    }
-
-    private void damagedCraft(@NotNull PlayerCraft craft, UUID playerUUID) {
+    static void damagedCraft(@NotNull PlayerCraft craft, UUID playerUUID) {
         UUID sender = playerUUID;
         Player cause = MovecraftCombat.getInstance().getServer().getPlayer(sender);
 
@@ -198,7 +163,7 @@ public class IgnitionListener implements Listener {
         Bukkit.getPluginManager().callEvent(new CraftDamagedByEvent(craft, damageRecord));
     }
 
-    private Craft fastNearestPlayerCraftToLoc(Location source) {
+    static Craft fastNearestPlayerCraftToLoc(Location source) {
         MovecraftLocation loc = MathUtils.bukkit2MovecraftLoc(source);
         Craft closest = null;
         long closestDistSquared = Long.MAX_VALUE;
@@ -215,5 +180,65 @@ public class IgnitionListener implements Listener {
         }
         return closest;
     }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onBlockBurnt(BlockBurnEvent event) {
+        final Block block = event.getBlock();
+        final Block volatileBlock = getNextBurnableBlock(block);
+        if (volatileBlock != null)
+            this.handleVolatile(event::setCancelled, volatileBlock, null, VolatileBlock.EReactionType.BLOCK_BURNT);
+    }
+
+    // Support volatile tnt too!
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onTNTPrimeEvent(TNTPrimeEvent event) {
+        // This is different, here, the TNT is the burnt block
+        if (event.getCause() != TNTPrimeEvent.PrimeCause.FIRE || event.getBlock() == null) {
+            return;
+        }
+
+        final Block block = event.getBlock();
+        this.handleVolatile(event::setCancelled, block, event.getPrimingEntity(), VolatileBlock.EReactionType.BLOCK_EXPLODED);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onIgnite(BlockIgniteEvent event) {
+        if (event.getCause() != BlockIgniteEvent.IgniteCause.FIREBALL)
+            return;
+
+        @Nullable
+        Player ignitingPlayer = null;
+        if (event.getIgnitingEntity() instanceof Player) {
+            ignitingPlayer = (Player) event.getIgnitingEntity();
+            if (ignitingPlayer.getInventory().getItemInMainHand().getType() == Material.FIRE_CHARGE ||
+                    ignitingPlayer.getInventory().getItemInOffHand().getType() == Material.FIRE_CHARGE) {
+                return;
+            }
+        }
+
+        Block sourceBlock = event.getBlock();
+        Block testBlock = getNextBurnableBlock(sourceBlock);
+        if (testBlock != null)
+            handleVolatile(event::setCancelled, testBlock, ignitingPlayer, VolatileBlock.EReactionType.BLOCK_CATCH_FIRE);
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onProjectileHit(ProjectileHitEvent event) {
+        if (event.getHitBlock() == null) {
+            return;
+        }
+
+        Entity shooter = null;
+        if (event.getEntity().getShooter() != null && event.getEntity().getShooter() instanceof Entity) {
+            shooter = (Entity) event.getEntity().getShooter();
+        }
+
+        final VolatileBlock.EReactionType reactionType = event.getEntity().getFireTicks() > 0 ? VolatileBlock.EReactionType.BLOCK_HIT_BY_BURNING_PROJECTILE : VolatileBlock.EReactionType.BLOCK_HIT_BY_PROJECTILE;
+        if (handleVolatile(event::setCancelled, event.getHitBlock(), shooter, reactionType)) {
+            event.getEntity().remove();
+        }
+
+    }
+
 }
 
